6e12858e9c61df7e64bff6ec73e387936d5f78d0
Anderson Souza <anderson@xnetweb.com.br>
2020-05-03 08:41:34 -0300

    Initialize repository with easy-ui5

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/myorg/firstApp/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.myorg.firstApp.controller.MainView", {});
+});
